08f99f886b034634259df3280b7c7f2246c60ff2
unknown <lallianpuiisr@gmail.com>
2026-05-06 22:48:07 +0530

    adb

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
             CourseSeeder::class,
             InstitutionSeeder::class,
             SemesterSeeder::class,
-            PeriodSeeder::class,
             WorkerPositionSeeder::class,
+            BcaViWeeklyTimetableSeeder::class,
         ]);
     }
 }
