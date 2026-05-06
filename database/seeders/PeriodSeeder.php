2bba29e976bc89cb3ab5fcbccf6c0d4af500ed81
unknown <lallianpuiisr@gmail.com>
2026-05-06 22:24:12 +0530

    adb

---
+++ database/seeders/PeriodSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Period;
+use App\Models\Semester;
+use Illuminate\Database\Seeder;
+
+class PeriodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $defaultPeriods = [
+            ['name' => 'Period 1', 'start_time' => '09:30', 'end_time' => '10:30', 'scan_window_minutes' => 5],
+            ['name' => 'Period 2', 'start_time' => '10:30', 'end_time' => '11:30', 'scan_window_minutes' => 5],
+            ['name' => 'Period 3', 'start_time' => '11:30', 'end_time' => '12:30', 'scan_window_minutes' => 5],
+            ['name' => 'Period 4', 'start_time' => '13:00', 'end_time' => '13:45', 'scan_window_minutes' => 5],
+            ['name' => 'Period 5', 'start_time' => '13:45', 'end_time' => '14:30', 'scan_window_minutes' => 5],
+            ['name' => 'Period 6', 'start_time' => '14:30', 'end_time' => '15:15', 'scan_window_minutes' => 5],
+        ];
+
+        $customPeriodsBySemester = [
+            36 => [
+                ['name' => 'Period 1', 'start_time' => '09:30', 'end_time' => '10:30', 'scan_window_minutes' => 5],
+                ['name' => 'Period 2', 'start_time' => '10:30', 'end_time' => '11:30', 'scan_window_minutes' => 5],
+                ['name' => 'Period 3', 'start_time' => '11:30', 'end_time' => '22:30', 'scan_window_minutes' => null],
+                ['name' => 'Period 4', 'start_time' => '13:00', 'end_time' => '14:00', 'scan_window_minutes' => 5],
+                ['name' => 'Period 5', 'start_time' => '14:00', 'end_time' => '15:00', 'scan_window_minutes' => 5],
+                ['name' => 'Period 6', 'start_time' => '14:30', 'end_time' => '15:15', 'scan_window_minutes' => 5],
+            ],
+        ];
+
+        $semesters = Semester::query()
+            ->select(['id', 'course_id'])
+            ->orderBy('id')
+            ->get();
+
+        foreach ($semesters as $semester) {
+            $periods = $customPeriodsBySemester[$semester->id] ?? $defaultPeriods;
+
+            foreach ($periods as $period) {
+                Period::updateOrCreate(
+                    [
+                        'course_id' => $semester->course_id,
+                        'semester_id' => $semester->id,
+                        'name' => $period['name'],
+                    ],
+                    [
+                        'start_time' => $period['start_time'],
+                        'end_time' => $period['end_time'],
+                        'scan_window_minutes' => $period['scan_window_minutes'],
+                        'is_active' => true,
+                    ]
+                );
+            }
+        }
+    }
+}
